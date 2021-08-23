6325e478e204cbf9c322e9b59e6089bdb8a42e75
Jamezjaz <jamezjaz@gmail.com>
2021-08-23 21:11:30 +0100

    feat: Adds counter to cart

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -3,32 +3,35 @@ import { Link } from 'react-router-dom';
 import NavStyles from './NavStyles.module.css';
 import logo from '../../assets/robot.jpg';
 import cart from '../../assets/cart.png';
+import { useSelector } from 'react-redux';
 
-const NavBar = () => (
-  <>
-    <nav className={NavStyles.nav}>
-      <h2 className="logo">
-        <Link to="/">
-          <img src={logo} alt={logo} />
-        </Link>
-        Robot Market
-      </h2>
-      <ul className={NavStyles.navLinks}>
-        <Link className={NavStyles.lists} to="/">
-          <li>Home</li>
-        </Link>
-        <Link className={NavStyles.lists} to="/about">
-          <li>About Us</li>
-        </Link>
-        <Link to="/cart">
-          <li>
-            <div className={NavStyles.counter}>0</div>
-            <img src={cart} className="w-75 pt-2" alt={cart} />
-          </li>
-        </Link>
-      </ul>
-    </nav>
-  </>
-);
-
+const NavBar = () => {
+  const counter = useSelector(state => state.robot.addedItems);
+  return (
+    <>
+      <nav className={NavStyles.nav}>
+        <h2 className="logo">
+          <Link to="/">
+            <img src={logo} alt={logo} />
+          </Link>
+          Robot Market
+        </h2>
+        <ul className={NavStyles.navLinks}>
+          <Link className={NavStyles.lists} to="/">
+            <li>Home</li>
+          </Link>
+          <Link className={NavStyles.lists} to="/about">
+            <li>About Us</li>
+          </Link>
+          <Link to="/cart">
+            <li>
+              <div className={NavStyles.counter}>{counter.length}</div>
+              <img src={cart} className="w-75 pt-2" alt={cart} />
+            </li>
+          </Link>
+        </ul>
+      </nav>
+    </>
+  );
+};
 export default NavBar;
