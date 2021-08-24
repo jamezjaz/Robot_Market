c1d32cec99f05067661f226d8319f0ab6ea98845
Jamezjaz <jamezjaz@gmail.com>
2021-08-24 21:38:20 +0100

    feat: Installs react-loader-spinner
    feat: Implements Spinner as long as loading is true

--- frontend/src/container/RobotList/RobotList.js
+++ frontend/src/container/RobotList/RobotList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import uuid from 'react-uuid';
+import Loader from "react-loader-spinner";
 import fetchRobots from '../../apiRequest/apiRequest';
 import Robot from '../../components/Robot/Robot';
 import RobotListStyles from './RobotListStyles.module.css';
@@ -13,9 +14,13 @@ const RobotList = props => {
   const { allRobots, fetchedRobots, filtered } = props;
   // console.log('all robots', allRobots);
   const [pageNumber, setPageNumber] = useState(0);
+  const [loading, setLoading] = useState(true);
   // const dispatch = useDispatch();
 
   useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
     fetchedRobots(allRobots);
   }, []);
 
@@ -43,25 +48,40 @@ const RobotList = props => {
         <h2 className="text-center mx-4">HOT DEALS!</h2>
         <RobotFilter handleFilter={handleFilterChange} />
       </div>
-      <table className="container-fluid d-flex flex-wrap">
-        {filteredRobots.map(robot => (
-          <Robot
-            key={uuid()}
-            robot={robot}
+      {loading 
+        ? (
+          <Loader
+            type="MutatingDots"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000} //3 secs
+            className="d-flex justify-content-center"
           />
-        ))}
-      </table>
-      <ReactPaginate
-        previousLabel={"Previous"}
-        nextLabel={"Next"}
-        pageCount={pageCount}
-        onPageChange={changePage }
-        containerClassName={RobotListStyles.paginationBtns}
-        previousLinkClassName={"previousBtn"}
-        nextLinkClassName={"nextBtn"}
-        disabledClassName={"paginateDisabled"}
-        activeClassName={RobotListStyles.paginationActive}
-      />
+        ) 
+        : (
+          <div>
+            <table className="container-fluid d-flex flex-wrap">
+              {filteredRobots.map(robot => (
+                <Robot
+                  key={uuid()}
+                  robot={robot}
+                />
+              ))}
+            </table>
+            <ReactPaginate
+              previousLabel={"Previous"}
+              nextLabel={"Next"}
+              pageCount={pageCount}
+              onPageChange={changePage }
+              containerClassName={RobotListStyles.paginationBtns}
+              previousLinkClassName={"previousBtn"}
+              nextLinkClassName={"nextBtn"}
+              disabledClassName={"paginateDisabled"}
+              activeClassName={RobotListStyles.paginationActive}
+            />
+          </div>
+        )}
     </>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import store from './redux/store/store';
 import App from './components/App/App';
 import './index.css';
